bf24a3541275a79aaa3ad1bec31d92a8d87d9094
Franco Insaurralde <69606146+Insaf99@users.noreply.github.com>
2021-05-09 23:54:51 -0300

    Add files via upload

---
+++ Bootcamproyect/src/Hens.java
@@ -0,0 +1,8 @@
+abstract class Hens{
+    public static String whites = "";
+    public static String reds = "";
+    egg Type;
+    }
+
+    //(i<((numberOfHens*70)/100) ? Egg.D : Egg.O, eggsPerDay)
+
---
+++ Bootcamproyect/src/Main.java
@@ -0,0 +1,19 @@
+/*You have to make a Farm, with a Farmer and a Hen House with 40 Hens,
+70% of Heans lay Red Eggs and 30% Heans lay White Eggs, each Hen will lay 2 eggs per day.
+Your Farmer have to recolect them inside an Egg's Carton, each Egg's Carton (6 x 5)
+has a capacity for 30 eggs and it will be classified them by color.
+Egg's Cartons Console representation in 1 day. e.g. (O): white eggs (D): red eggs (G): empty
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to the farm!");
+
+        HenHouse hen = new HenHouse(); // instance of class
+        farmer Farmer = farmer.getInstance("messi");
+
+        Farmer.diference("reds");
+        Farmer.diference("whites");
+        Farmer.diference("blues");
+
+    }
+}
